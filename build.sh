e9af9ae6b41fe46c3a638c1d2046ee527836099f
Clark Gaebel <cg.wowus.cg@gmail.com>
2011-07-27 21:14:22 -0400

    Added clang analysis to the build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Usage: CC=gcc ./build.sh | CC=clang ./build.sh etc, etc.
 
-$CC -Wall -Wextra -Werror -pipe -pedantic -std=c99 -DFORTIFY_SOURCE=2 -O2 -c arena.c
+$CC -Wall -Wextra -Werror -pipe -pedantic -std=c99 -DFORTIFY_SOURCE=2 -O3 -march=native -c arena.c
+clang --analyze -std=c99 -Wall -Wextra -Werror -pipe -pedantic arena.c
